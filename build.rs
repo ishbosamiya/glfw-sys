e5326a4b5974ab67690c301cd5ae098d54be3bc1
ishbosamiya <ishbosamiya@gmail.com>
2024-04-14 14:31:30 +0530

    build: define CMAKE_MSVC_RUNTIME_LIBRARY=MultiThreaded for windows build
    Without this definition, it leads to linker errors. See code comments
    for further details.
    
    NOTE: the linker error is not caught by the CI, unsure why

--- build.rs
+++ build.rs
@@ -9,6 +9,25 @@ fn main() {
         .define("GLFW_BUILD_DOCS", "OFF")
         .define("CMAKE_INSTALL_LIBDIR", "lib");
 
+    // requires multithreaded runtime for static library (for
+    // windows), otherwise there are linker errors with unresolved
+    // external symbols
+    //
+    // TODO: need to figure out why `cfg.static_crt(true)` does not
+    // work but setting `CMAKE_MSVC_RUNTIME_LIBRARY` to
+    // `MultiThreaded` or `MultiThreadedDebug`
+    // does. `static_crt(true)` causes `cc` (`cmake` internally passes
+    // along `static_crt` to `cc`) to emit `-MT` flag, which is the
+    // same as `CMAKE_MSVC_RUNTIME_LIBRARY` based on its
+    // documentation.
+    //
+    // reference: <https://github.com/glfw/glfw/issues/1981>
+    #[cfg(target_os = "windows")]
+    cfg.define(
+        "CMAKE_MSVC_RUNTIME_LIBRARY",
+        "MultiThreaded$<$<CONFIG:Debug>:Debug>",
+    );
+
     let dst = if cfg!(feature = "wayland") {
         cfg.define("GLFW_USE_WAYLAND", "ON").build()
     } else {
